e5677867e047e13aac9a9bbd6564833695b61a5d
oBeta97 <paolopellizzari97@gmail.com>
2024-11-08 15:45:21 +0100

    forbidden commit i know, but this is a hot fix :P

--- src/main/java/PaoloPellizzari/progettosettimana19/exceptions/ExceptionsHandler.java
+++ src/main/java/PaoloPellizzari/progettosettimana19/exceptions/ExceptionsHandler.java
@@ -3,16 +3,17 @@ package PaoloPellizzari.progettosettimana19.exceptions;
 
 import PaoloPellizzari.progettosettimana19.payloads.ErrorsResponseDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class ExceptionsHandler {
 
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class) // Tra le parentesi indico quale eccezione dovrà essere gestita da questo metodo
+    @ExceptionHandler(BadRequestException.class) // Tra le parentesi indico quale eccezione dovrà essere gestita da questo metodo
     @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
     public ErrorsResponseDTO handleBadrequest(BadRequestException ex) {
         return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
@@ -30,13 +31,13 @@ public class ExceptionHandler {
 //        return new ErrorsResponseDTO("Non hai i permessi per accedere", LocalDateTime.now());
 //    }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND) // 404
     public ErrorsResponseDTO handleNotFound(NotFoundException ex) {
         return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
     public ErrorsResponseDTO handleGeneric(Exception ex) {
         ex.printStackTrace(); // Non dimentichiamoci che è estremamente utile sapere dove è stata generata un'eccezione per poterla facilmente fixare
