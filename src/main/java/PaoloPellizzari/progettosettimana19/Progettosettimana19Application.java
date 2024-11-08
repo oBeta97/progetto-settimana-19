2e1f016057e6f88abc2d69c8b884971e4041411a
oBeta97 <paolopellizzari97@gmail.com>
2024-11-08 11:59:01 +0100

    created project and added gitignore that was missing

---
+++ src/main/java/PaoloPellizzari/progettosettimana19/Progettosettimana19Application.java
@@ -0,0 +1,13 @@
+package PaoloPellizzari.progettosettimana19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettosettimana19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettosettimana19Application.class, args);
+	}
+
+}
